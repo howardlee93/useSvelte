68895f627ee79a938c13e5ec204f3fdbbb79953f
Howard <howardl93@yahoo.com>
2020-09-08 15:33:27 -0700

    will need to deal with cors issue

--- src/util/api_util.js
+++ src/util/api_util.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 const baseUrl = 'https://usesthis.com/api/stats';
 
 export const fetchInfo = (type, query) =>( 
-    axios.get(baseUrl+`/${type}/${query}`)
+    axios.get(baseUrl+`/${type}/${query}`,
+    {headers: {'Access-Control-Allow-Origin': '*'}}
+)
 
 );
